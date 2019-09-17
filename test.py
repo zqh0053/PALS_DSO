4415b4b1ae309d1f1764bf9c692d02c1ba184ed2
zqh0053 <zqh0053@gmail.com>
2019-09-17 10:11:26 +0800

    0917

--- test.py
+++ test.py
@@ -1,63 +1,66 @@
-# import numpy as np
-# from numpy.linalg import cholesky
-# import tools as tl1
-# import matplotlib.pyplot as plt
-# import time
-# sampleNo = 1000000
-# mu = 85
-# sigma = 4
-# np.random.seed(0)
-# s = np.random.normal(mu, sigma, sampleNo)
-# t0_1 = time.time()
-# #h1 = plt.hist(s, 300,range=(0, 300))
-# c = [-10,-5,0,5,3,10,15,-20,25]
-# b = [10,23,20]
-# d = [c,b]
-# print(d)
-# print(c.index(-10))
-# print(d[1][1])
-# t0_2 = time.time()
-# print(t0_2 - t0_1)
-# tm1 = (0.01, 0.02)
-# tm5 = tuple(c)
-# print(tm5)
-# tm2 = np.array(tm1)
-# tm3 = -1. * tm2
-# print(tm3)
-# # = h1[0].tolist()
-# filename1 = 'C2--C1C2_1200M--00000.csv'
-# k = 0
-# time_1 = []
-# volt_1 = []
-# with open(filename1) as file1_object:
-#     for lines in file1_object:
-#         if k < 5:
-#             k = k + 1
-#         else:
-#             line = lines.rstrip().split(',')
-#             time_1.append(1000000000 * float(line[0]))
-#             volt_1.append(float(line[1]))
-# # print(time_1)
-#
-# tr1 = tl1.Wavetools(time_1, volt_1, basenum=9000)
-# print(tr1.find_baseline())
-# t_0 = time.time()
-# print(tr1.get_time_cfd_linear(0.2))
-#
-# print(tr1.get_time_cfd_poln(0.2, 4, 0.4))
-# t_1 = time.time()
-# print(t_1 - t_0)
-# print(tr1.get_slope())
-# l1 = plt.plot(time_1, volt_1)
-# plt.show()
-#
-# str1 = 'asd'
-# str2 = 1
-# str3 = "asd " + str(str2)
-# print(str3)
+import numpy as np
+from numpy.linalg import cholesky
+import tools as tl1
+import matplotlib.pyplot as plt
+import time
+sampleNo = 1000000
+mu = 85
+sigma = 4
+np.random.seed(0)
+s = np.random.normal(mu, sigma, sampleNo)
+t0_1 = time.time()
+#h1 = plt.hist(s, 300,range=(0, 300))
+c = [-10,-5,0,5,3,10,15,-20,25]
+b = [10,23,20]
+d = [c,b]
+print(d)
+print(c.index(-10))
+print(d[1][1])
+t0_2 = time.time()
+print(t0_2 - t0_1)
+tm1 = (0.01, 0.02)
+tm5 = tuple(c)
+print(tm5)
+tm2 = np.array(tm1)
+tm3 = -1. * tm2
+print(tm3)
+# = h1[0].tolist()
+filename1 = 'C2--C1C2_1200M--00000.csv'
+k = 0
+time_1 = []
+volt_1 = []
+with open(filename1) as file1_object:
+    for lines in file1_object:
+        if k < 5:
+            k = k + 1
+        else:
+            line = lines.rstrip().split(',')
+            time_1.append(1000000000 * float(line[0]))
+            volt_1.append(float(line[1]))
+# print(time_1)
 
-import wx
-import os
+tr1 = tl1.Wavetools(time_1, volt_1, basenum=9000)
+print(tr1.find_baseline())
+t_0 = time.time()
+for i in range(0, 100000):
+    # tr1.get_time_cfd_linear(0.2)
+    tr1.get_time_cfd_poln(0.2, 3, 0.4)
+    # print(tr1.get_time_cfd_linear(0.2))
+    # print(tr1.get_time_cfd_poln(0.2, 4, 0.4))
+t_1 = time.time()
+print('time')
+print(t_1 - t_0)
+# print(tr1.get_slope())
+l1 = plt.plot(time_1, volt_1)
+plt.show()
+
+str1 = 'asd'
+str2 = 1
+str3 = "asd " + str(str2)
+print(str3)
+
+# import wx
+# import os
 
 # class MainWindow(wx.Frame):
 #     def __init__(self, parent, title, size=(600, 400)):
@@ -149,16 +152,16 @@ import os
 # plt.title('polyfitting')
 # plt.show()
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-np.random.seed(19601)
-data = np.random.randn(2, 100)
-
-fig, axs = plt.subplots(2, 2, figsize=(5, 5))
-axs[0, 0].hist(data[0])
-axs[1, 0].scatter(data[0], data[1])
-axs[0, 1].plot(data[0], data[1])
-axs[1, 1].hist2d(data[0], data[1])
-
-plt.show()
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# np.random.seed(19601)
+# data = np.random.randn(2, 100)
+#
+# fig, axs = plt.subplots(2, 2, figsize=(5, 5))
+# axs[0, 0].hist(data[0])
+# axs[1, 0].scatter(data[0], data[1])
+# axs[0, 1].plot(data[0], data[1])
+# axs[1, 1].hist2d(data[0], data[1])
+#
+# plt.show()
